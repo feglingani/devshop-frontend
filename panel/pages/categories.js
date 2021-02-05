a54430bf79a65d306cfc25f468de20af06779daa
Fernando Glingani <fernando.glingani@gmail.com>
2021-02-05 18:46:50 -0300

    add components

---
+++ panel/pages/categories.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Layout from '../components/Layout';
+import Title from '../components/Title';
+
+const Index = () => {
+  return (
+		<Layout>
+			<Title>Categorias</Title>
+		</Layout>
+  )
+}
+
+export default Index
